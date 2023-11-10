7fbab2f011a138bf8dccc4ec51a9fd57e425502b
vixelina <53301959+Sinily@users.noreply.github.com>
2023-11-11 01:52:25 +0200

    Delete stdafx.cpp

--- stdafx.cpp
+++
@@ -1,9 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Win32Project1.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-// TODO: reference any additional headers you need in STDAFX.H
-#pragma comment(lib, "GdiPlus.lib")
-// and not in this file
